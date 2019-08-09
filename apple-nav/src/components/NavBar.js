c68d3358048181f17c3a9b7a3a44e9d7d9217708
MicahJank <MicahJank@users.noreply.github.com>
2019-08-09 18:16:13 -0500

    added icons to the navbar for certain menu selections

--- apple-nav/src/components/NavBar.js
+++ apple-nav/src/components/NavBar.js
@@ -4,15 +4,29 @@ import { NavLink } from 'react-router-dom';
 
 import styled from 'styled-components';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { } from '@fortawesome/fontawesome-svg-core';
+import { faApple } from '@fortawesome/free-brands-svg-icons';
+ import { faSearch, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+
+const CoffeeIcon = <FontAwesomeIcon icon={faApple} />
+
 const StyledLink = styled.div`
     color: #FAFAFA;
     font-size: 1rem;
-    padding: 14px;
+    padding: 12px 22px;
+
+`;
+
+const Icon = styled.span`
+    font-size: 1.3rem;
 `;
 
 const NavContainer = styled.div`
   width: 100%;
   display: flex;
+  justify-content: space-between;
+  align-items: baseline;
   background-color: #323232;
 `;
 
@@ -21,7 +35,7 @@ const NavBar = () => {
 
     return (
         <NavContainer>
-             <NavLink> <StyledLink>Mac</StyledLink> </NavLink>
+             <NavLink> <StyledLink> <Icon><FontAwesomeIcon icon={faApple} /> </Icon></StyledLink> </NavLink>
              <NavLink> <StyledLink>Mac</StyledLink> </NavLink>
              <NavLink> <StyledLink>iPad</StyledLink> </NavLink>
              <NavLink> <StyledLink>iPhone</StyledLink> </NavLink>
@@ -29,8 +43,8 @@ const NavBar = () => {
              <NavLink> <StyledLink>TV</StyledLink> </NavLink>
              <NavLink> <StyledLink>Music</StyledLink> </NavLink>
              <NavLink> <StyledLink>Support</StyledLink> </NavLink>
-             <NavLink> <StyledLink>Mac</StyledLink> </NavLink>
-             <NavLink> <StyledLink>Mac</StyledLink> </NavLink>
+             <NavLink> <StyledLink><Icon><FontAwesomeIcon icon={faSearch} /></Icon></StyledLink> </NavLink>
+             <NavLink> <StyledLink><Icon><FontAwesomeIcon icon={faShoppingBag} /></Icon></StyledLink> </NavLink>
         </NavContainer>
        
   
