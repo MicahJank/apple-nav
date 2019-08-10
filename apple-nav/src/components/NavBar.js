8079f36ecf49d1ed7ebb5ec03d4a1a557e638a88
MicahJank <MicahJank@users.noreply.github.com>
2019-08-09 19:02:30 -0500

    added some transition effects for the hovering of the links

--- apple-nav/src/components/NavBar.js
+++ apple-nav/src/components/NavBar.js
@@ -2,21 +2,27 @@ import React from 'react';
 
 import { NavLink } from 'react-router-dom';
 
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { Tween } from 'react-gsap';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { } from '@fortawesome/fontawesome-svg-core';
 import { faApple } from '@fortawesome/free-brands-svg-icons';
- import { faSearch, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
-
-const CoffeeIcon = <FontAwesomeIcon icon={faApple} />
+import { faSearch, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 
 const StyledLink = styled.div`
     color: #FAFAFA;
     font-size: 1rem;
     padding: 12px 22px;
-
+    transition: color 0.3s;
+    
+    :hover {
+        color: #B7B7B7;
+        transition: color 0.3s;
+    }
 `;
+
+
 
 const Icon = styled.span`
     font-size: 1.3rem;
