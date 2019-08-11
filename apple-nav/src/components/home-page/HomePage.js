2f65688b7b0f8e3a295856795a327d9af398603e
MicahJank <MicahJank@users.noreply.github.com>
2019-08-11 11:17:17 -0500

    refactored the xrheader into its own component

--- apple-nav/src/components/home-page/HomePage.js
+++ apple-nav/src/components/home-page/HomePage.js
@@ -4,17 +4,17 @@ import styled, { keyframes } from 'styled-components';
 import { Tween } from 'react-gsap';
 
 // import sections of home page here
-import XrHeader from './imgs/XR-Header.png'; // TODO - change this into a sub component and then add more for the different sections
-
+import XrHeader from './XRHeader.js';
 
 const HomePage = () => {
-
-    return (
-        <Tween from={{x: '200px', opacity: 0} } duration={0.5}>
-            <img src={XrHeader} alt='iphone homepage xr header' /> 
-        </Tween>
-        
-    );
+  return (
+    <Tween from={{ x: '200px', opacity: 0 }} duration={0.5}> 
+        <div>
+         <XrHeader />
+        </div>
+    </Tween> 
+   
+  );
 };
 
 export default HomePage;
